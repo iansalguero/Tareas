fac83686e5acde72cd0b06bc1eedbe9821286251
iansalguero <114033098+iansalguero@users.noreply.github.com>
2022-09-30 12:02:45 -0600

    Add files via upload

---
+++ L1_IPSW_1151222/L1_IPSW_1151222/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L1_IPSW_1151222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su nombre : ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Soy " + nombre );
+
+            Console.Write("Hola Mundo ");
+            Console.Write("Soy " + nombre);
+
+            Console.ReadKey();
+        }   
+    }
+}
---
+++ Tareas/Tareas/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi segundo programa");
+            Console.WriteLine();
+            Console.WriteLine();
+            string nombre, edad, carrera, carnet;
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            edad = Console.ReadLine();
+            Console.WriteLine("Ingrese su carrera");
+            carrera = Console.ReadLine();
+            Console.WriteLine("Ingrese su carné");
+            carnet = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: "+edad);
+            Console.WriteLine("Carrera: "+carrera);
+            Console.WriteLine("Carné: "+carnet);
+            Console.WriteLine();
+            Console.WriteLine("Soy " + nombre + " tengo " + edad + " años y estudio la carrera de " + carrera +"." );
+            Console.WriteLine("Mi numero de carné es " + carnet +".");
+            Console.ReadKey();
+        }
+    }
+}
